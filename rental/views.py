8306484183650e6b35eef7c989b49e690d5f5927
Hasan Unlu <hu1997tr@gmail.com>
2023-11-29 21:38:41 +0300

    Created the basic template for the basic views

--- rental/views.py
+++ rental/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Uav, Brand, UavInstance, Category
+
+def index(request):
+
+    num_uav_models = Uav.objects.all().count()
+    num_instances = UavInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = UavInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_brands = Brand.objects.count()
+
+    context = {
+        'num_uav_models': num_uav_models,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_brands': num_brands,
+    }
+
+    return render(request, 'index.html', context=context)
